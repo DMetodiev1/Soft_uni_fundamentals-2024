ad1485ab01cbfedc07492be04b6f20fc25e58972
DMetodiev1 <dmskeepout@gmail.com>
2024-05-23 22:54:19 +0300

    add a row

--- venv/Fundamentals/wolf_in_sheep's_clothing.py
+++ venv/Fundamentals/wolf_in_sheep's_clothing.py
@@ -6,3 +6,4 @@ for index, animal in enumerate(animals_list):
         print('Please go away and stop eating my sheep')
     elif animal == 'wolf':
         print(f'Oi! Sheep number {index}! You are about to be eaten by a wolf!')
+        print("Sisi ima malko pisi")
