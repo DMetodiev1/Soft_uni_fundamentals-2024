1d4ef751523c61f7d9e2623effb42ede54d26325
DMetodiev1 <dmskeepout@gmail.com>
2024-05-23 07:57:10 +0300

    added new row

---
+++ venv/Fundamentals/sorting_hat.py
@@ -0,0 +1,20 @@
+names = input()
+while True:
+    if names == "Welcome!":
+        print(f"Welcome to Hogwarts.")
+        break
+    elif names == "Voldemort":
+        print(f"You must not speak of that name!")
+        break
+    char = int(len(names))
+    if char < 5:
+        print(f"{names} goes to Gryffindor.")
+    if char == 5:
+        print(f"{names} goes to Slytherin.")
+    if char == 6:
+        print(f"{names} goes to Ravenclaw.")
+    if char > 6:
+        print(f"{names} goes to Hufflepuff.")
+    names = input()
+
+# how to commit in pyCharm
